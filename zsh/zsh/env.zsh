593a040662eee91ae9af13d7b33e9226016f28d6
zagger <huangzijie@os-easy.com>
2021-04-15 13:36:27 +0800

    备份zsh

---
+++ zsh/zsh/env.zsh
@@ -0,0 +1,40 @@
+###
+ # @Description: 
+ # @Author: zagger
+ # @Date: 2021-03-20 11:33:39
+ # @LastEditors: zagger
+ # @LastEditTime: 2021-03-20 11:40:40
+### 
+
+#unuse
+#export LOCALPROG=$HOME/prog
+#export GOPATH=$HOME/go
+#export PATH=$PATH:$HOME/.gem/ruby/2.6.0
+#export PATH=$PATH:$HOME/go/bin
+#export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+#export PATH=$PATH:/home/linuxbrew/.linuxbrew/sbin
+#export PATH=$PATH:$HOME/.linuxbrew/bin
+#export PATH=$PATH:$HOME/.linuxbrew/sbin
+#export PATH=$PATH:$HOME/.local/bin
+#export PATH=$PATH:$HOME/.cargo/bin
+#export PATH=$PATH:/usr/local/opt/node@12/bin
+#export TERM=xterm-256color
+#export PATH=$PATH:$LOCALPROG/flutter/bin
+#export PATH=$PATH:$LOCALPROG/flutter/bin/cache/dart-sdk/bin
+#export PATH="$PATH":"$HOME/.pub-cache/bin"
+#export FLUTTER_ROOT=$LOCALPROG/flutter
+#export TERM=screen-256color
+
+export XDG_CONFIG_HOME=$HOME/.config
+export LOCALBIN=$XDG_CONFIG_HOME/bin
+export PATH=$PATH:$LOCALBIN
+export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:/usr/local/Cellar/node/15.0.1/bin
+export PATH=$PATH:/usr/local/Cellar/node/14.2.0/bin
+export PATH=$PATH:/snap/bin
+export TERM=xterm-256color
+export TERM_ITALICS=true
+export RANGER_LOAD_DEFAULT_RC="false"
+export EDITOR=nvim
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
